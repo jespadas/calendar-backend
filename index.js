85292706439f16bac8dcd7ed9c31c2962f6fed2d
jespadas <julio.espadas@gmail.com>
2021-10-06 13:18:31 +0200

    database and cors added

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
+const { dbConnection } = require( './database/config' );
 
 // Create server
 const app = express();
 
+// Database
+dbConnection();
+
+// CORS
+app.use(cors());
+
 // Public directory
 app.use(express.static('public'));
+
+// Body parser
+app.use(express.json());
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
